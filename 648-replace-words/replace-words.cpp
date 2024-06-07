3ac9c0daca64c3b42e0da10e852f36ded0c90525
Debanjan Konar <91771502+Uni-Bo@users.noreply.github.com>
2024-06-07 11:35:41 +0530

    Time: 479 ms (25.46%) | Memory: 335.4 MB (18.95%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,32 +1,41 @@
-class Solution {
+class Solution{
 public:
-    string replaceWords(vector<string>& dictionary, string sentence) {
-        set<string>set;
-        set.insert(dictionary.begin(),dictionary.end());
-        string temp="",ans="";
-        for(int i=0;i<sentence.size();i++)
-        {
-            if(sentence[i]==' '){
-                ans+=temp+" ";
-                temp="";
-                i++;
-            }
+bool startsWith(const string &word, const string &prefix) {
+    if (word.size() < prefix.size()) return false;
+    return word.substr(0, prefix.size()) == prefix;
+}
 
-            temp=temp+sentence[i];
-            //to check if the value is present in set (i.e. dictionary)
-            if(set.count(temp))
-            {
-                while(i<sentence.size() && sentence[i]!=' ')  i++;
-                if(sentence[i]==' ')
-                    ans+=temp+" ";
-                else
-                    ans+=temp;
-                temp="";
-            }
-        }
+string replaceWords(vector<string>& dictionary, string sentence) {
+    // Sort the dictionary by the length of the roots
+    sort(dictionary.begin(), dictionary.end(), [](const string &a, const string &b) {
+        return a.size() < b.size();
+    });
 
-        if(temp.size()) ans+=temp;
-        return ans;
+    // Split the sentence into words
+    istringstream iss(sentence);
+    string word;
+    vector<string> words;
+    while (iss >> word) {
+        words.push_back(word);
     }
 
+    // Iterate over each word and replace it with the appropriate root if possible
+    for (auto &word : words) {
+        for (const auto &root : dictionary) {
+            if (startsWith(word, root)) {
+                word = root;
+                break; // Once a root is found, no need to check further
+            }
+        }
+    }
+
+    // Join the words back into a sentence
+    ostringstream oss;
+    for (size_t i = 0; i < words.size(); ++i) {
+        if (i != 0) oss << " ";
+        oss << words[i];
+    }
+
+    return oss.str();
+}
 };
