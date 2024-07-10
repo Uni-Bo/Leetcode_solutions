6b2c96777324564c27bd5ff68513852fd9a19a7b
Debanjan Konar <91771502+Uni-Bo@users.noreply.github.com>
2024-07-10 13:01:48 +0530

    Time: 9 ms (22.38%) | Memory: 14.2 MB (31.02%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -4,10 +4,8 @@ public:
         stack<string>stk;
         for(int i=0;i<logs.size();i++)
         {
-            cout<<stk.size()<<"\n";
             if(logs[i]=="../")
-            {
-                
+            {   
                 if(stk.empty()) continue;
                 else stk.pop();
             }
