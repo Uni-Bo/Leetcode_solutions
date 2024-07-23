95313234bef36df5d6ebc78ca0509628937ab0e2
Debanjan Konar <91771502+Uni-Bo@users.noreply.github.com>
2024-07-23 11:23:26 +0530

    Time: 7 ms (55.39%) | Memory: 14.5 MB (81.61%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    
-    static bool cmp(pair<int,int>&a, pair<int,int>&b) {
-        return  (a.second==b.second) ? a.first>b.first : a.second<b.second;
-    }
-    
-    
-    vector<int> frequencySort(vector<int>& nums) {
-        if(nums.size()==1) 
-            return nums;
-        map<int,int> mp;
-        for(int i=0;i<nums.size();i++) 
+    vector<int> frequencySort(vector<int>& nums) 
+    {
+        map<int, int> frequencies;
+
+        for(int number: nums)
         {
-            mp[nums[i]]++;
+            frequencies[number] += 1;
         }
-        vector<pair<int,int>> val_freq;
-        for(auto m : mp) 
+
+        vector<pair<int, int>> frequencyPairs(frequencies.begin(), frequencies.end());
+    
+    
+        sort(frequencyPairs.begin(), frequencyPairs.end(), [=](pair<int, int> a, pair<int, int> b) 
         {
-            val_freq.push_back(m);
-        }
-        sort(val_freq.begin(),val_freq.end(),cmp);
-        vector<int> result;
-        for(auto v : val_freq) {
-            for(int i=0;i<v.second;i++) {
-                result.push_back(v.first);
+            if (a.second == b.second) 
+                return a.first > b.first;
+            
+            return a.second < b.second;
+        });
+
+        vector<int> sortedElements;
+        for (auto& [num, count] : frequencyPairs) 
+        {
+            for (int i = 0; i < count; ++i) 
+            {
+                sortedElements.push_back(num);
             }
         }
-        return result;
+
+        return sortedElements;
+
+
+        
     }
 };
