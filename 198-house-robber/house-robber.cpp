5eb639e04fad0284b4626e8bae4766a2b9a8f2fe
Debanjan Konar <91771502+Uni-Bo@users.noreply.github.com>
2024-08-13 20:52:50 +0530

    Time: 4 ms (31.62%) | Memory: 9.4 MB (36.29%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-int n;
-vector<int> dp;
-    int solve(int ind, vector<int> &nums) {
-    if (ind < 0)
-        return 0;
-    // what at index =0 ->mean
-
-    if (ind == 0)
-        return nums[ind];
-
-    if(dp[ind] != -1)return dp[ind];
-    int pick = solve(ind - 2,nums) + nums[ind];
-    int notPick = solve(ind - 1,nums) + 0;
-
-    return dp[ind] =max(pick, notPick);
-    }
     int rob(vector<int>& nums) {
-        n =nums.size();
-        dp.assign(n+1,-1);
-        return solve(n-1,nums);
+        int n=nums.size();
+        vector<int>dp(n,0);
+        if(n==0)return 0;
+        if(n==1)return nums[0];
+        dp[0]=nums[0];//1st house robbed
+        dp[1]=max(nums[0],nums[1]);//1 house is robbed, but i need to decide which one better
+        for(int i=2;i<n;i++)
+        {
+            //i represents the number of houses robbed
+            dp[i]=max(
+                //if I dont rob this house, I could have robbed the adjecent house
+                dp[i-1],
+                //if I rob this house, i get the valuable, but cant have adjacent house
+                dp[i-2]+nums[i]
+            );
+        }
+        return dp[n-1];
     }
 };
