0e98ed9045d74b5b64036bb5fa2515ea0129c356
Debanjan Konar <91771502+Uni-Bo@users.noreply.github.com>
2024-12-01 19:44:58 +0530

    Time: 34 ms (50.23%) | Memory: 73.6 MB (29.32%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int>set;
+        unordered_set<int>set;
         for(auto n:nums){
             if(set.find(n)==set.end()) set.insert(n);
             else return true;
